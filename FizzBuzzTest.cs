79d678e8af4ef4ac5bde68b5a2b3e6eb9e3813c2
SimonSebright <simonsebright@hotmail.com>
2019-11-14 16:24:50 +0100

    Multiples

--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -7,21 +7,27 @@ namespace FizzBuzz01
     {
         internal static string Eval(int v)
         {
-            return v == 3 ? "Fizz" : 
-                   v == 5 ? "Buzz" : v.ToString();
+            return v%3 == 0 ? "Fizz" : 
+                   v%5 == 0 ? "Buzz" : v.ToString();
         }
     }
     [TestClass]
     public class FizzBuzzTest
     {
         [TestMethod]
-        public void Start()
+        public void Singles()
         {
             Assert.AreEqual("1", FizzBuzzer.Eval(1));
             Assert.AreEqual("2", FizzBuzzer.Eval(2));
             Assert.AreEqual("Fizz", FizzBuzzer.Eval(3));
             Assert.AreEqual("4", FizzBuzzer.Eval(4));
             Assert.AreEqual("Buzz", FizzBuzzer.Eval(5));
+        }
+        [TestMethod]
+        public void Multiples()
+        {
+            Assert.AreEqual("Fizz", FizzBuzzer.Eval(6));
+            Assert.AreEqual("Buzz", FizzBuzzer.Eval(10));
         }
     }
 }
