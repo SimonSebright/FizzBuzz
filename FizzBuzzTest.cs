999cd039142d75f1992cdc7481da1a7fc26bd99d
SimonSebright <simonsebright@hotmail.com>
2019-11-14 16:21:11 +0100

    got to 5

--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -5,9 +5,10 @@ namespace FizzBuzz01
 {
     static class FizzBuzzer
     {
-        internal static object Eval(int v)
+        internal static string Eval(int v)
         {
-            return v == 3 ? (object)"Fizz" : v;
+            return v == 3 ? "Fizz" : 
+                   v == 5 ? "Buzz" : v.ToString();
         }
     }
     [TestClass]
@@ -16,9 +17,11 @@ namespace FizzBuzz01
         [TestMethod]
         public void Start()
         {
-            Assert.AreEqual(1, FizzBuzzer.Eval(1));
-            Assert.AreEqual(2, FizzBuzzer.Eval(2));
+            Assert.AreEqual("1", FizzBuzzer.Eval(1));
+            Assert.AreEqual("2", FizzBuzzer.Eval(2));
             Assert.AreEqual("Fizz", FizzBuzzer.Eval(3));
+            Assert.AreEqual("4", FizzBuzzer.Eval(4));
+            Assert.AreEqual("Buzz", FizzBuzzer.Eval(5));
         }
     }
 }
